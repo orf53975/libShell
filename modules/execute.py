ca966761e2c17300bec573ac697e44a6689aa76d
ahhh <ahhh.db@gmail.com>
2015-04-08 16:41:41 -0700

    Update execute.py

--- modules/execute.py
+++ modules/execute.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 def hello():
 	print 'hello world'
+	
+def execute(command):
+	proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+	stdoutput = proc.stdout.read() + proc.stderr.read()
+	return stdoutput
