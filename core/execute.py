267fda9cf79a775ac227d1e94f07a971c62eaa01
ahhh <ahhh.db@gmail.com>
2015-04-08 18:09:22 -0700

    Update execute.py

--- core/execute.py
+++ core/execute.py
@@ -5,5 +5,4 @@ import subprocess
 def shell_command(command):
 	proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 	stdoutput = proc.stdout.read() + proc.stderr.read()
-	print stdoutput
 	return stdoutput
